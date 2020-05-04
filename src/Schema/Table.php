24be4e3df3a512471a889911b5dd807d84c3eec5
Guilherme Blanco <guilhermeblanco@gmail.com>
2020-05-04 17:56:51 -0400

    Moved back methods

--- src/Schema/Table.php
+++ src/Schema/Table.php
@@ -963,19 +963,6 @@ class Table extends AbstractAsset
         }
     }
 
-    public function setComment(?string $comment) : self
-    {
-        // For keeping backward compatibility with MySQL in previous releases, table comments are stored as options.
-        $this->addOption('comment', $comment);
-
-        return $this;
-    }
-
-    public function getComment() : ?string
-    {
-        return $this->_options['comment'] ?? null;
-    }
-
     /**
      * @param string[] $columnNames
      * @param string[] $flags
@@ -1016,5 +1003,18 @@ class Table extends AbstractAsset
         }
 
         return $this->trimQuotes(strtolower($identifier));
+    }
+
+    public function setComment(?string $comment) : self
+    {
+        // For keeping backward compatibility with MySQL in previous releases, table comments are stored as options.
+        $this->addOption('comment', $comment);
+
+        return $this;
+    }
+
+    public function getComment() : ?string
+    {
+        return $this->_options['comment'] ?? null;
     }
 }
